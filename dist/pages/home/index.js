e76a7c9c7c227069db8101b398df67a4daf7f550
shanzx <425002410@qq.com>
2018-07-09 22:58:41 +0800

    new wx project

--- dist/pages/home/index.js
+++ dist/pages/home/index.js
@@ -1,15 +1,24 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 exports.default = Page({
-  data: {
-    isShow: false
-  },
-  starShow: function starShow() {
-    this.setData({
-      isShow: true
-    });
-  }
+    data: {
+        current11: 0,
+        items4: ['http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg', 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg']
+
+    },
+    handleChange11: function handleChange11(e) {
+        var index = e.detail.index;
+        this.setData({
+            current11: index
+        });
+    },
+    handleContentChange11: function handleContentChange11(e) {
+        var current = e.detail.current;
+        this.setData({
+            current11: current
+        });
+    }
 });
