eb9787a6ebdf2e2f138f4142ee2ca9564e3309eb
Muzi <425002410@qq.com>
2018-07-26 19:14:36 +0800

    chonggou

--- dist/pages/home/index.js
+++ dist/pages/home/index.js
@@ -5,32 +5,43 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = Page({
   data: {
-    listNames: ["暂无", "暂无", "暂无"],
+    listNames: null, //导航列表
     current11: 0,
-    current: 0,
-    items4: ["../../img/swiper1.png", "../../img/swiper2.png", "../../img/swiper3.png"],
-    column11: null,
-    pictureList: null,
-    titles: null
+    current: 0, //轮播图的
+    items4: ["../../img/swiper1.png", "../../img/swiper2.png", "../../img/swiper3.png"], //轮播图
+    colume: null,
+    column11: null, //列表data
+    column12: null,
+    column13: null,
+    pictureList: null, //二维码
+    titles: null //标题
   },
-  handleShow: function handleShow() {
-    var allData = this.data.column11;
+  handleShow: function handleShow(e) {
+    var allData = this.data.column;
     for (var i = 0; i < allData.length; i++) {
-      allData[i].show = false;
+      var ids = allData[i].content;
+      for (var j = 0; j < ids.length; j++) {
+        ids[j].show = false;
+      }
     }
     this.setData({
-      column11: allData
+      column: allData
     });
   },
+  //弹框点击x号触发的事件（让所有的show变为false，即所有的弹框都是隐藏，防止下次弹出时有两个为true）
   maskhide: function maskhide() {
-    var allData = this.data.column11;
+    var allData = this.data.column;
     for (var i = 0; i < allData.length; i++) {
-      allData[i].show = false;
+      var ids = allData[i].content;
+      for (var j = 0; j < ids.length; j++) {
+        ids[j].show = false;
+      }
     }
     this.setData({
-      column11: allData
+      column: allData
     });
   },
+  //弹框点击mask遮罩层触发的事件（让所有的show变为false，即所有的弹框都是隐藏，防止下次弹出时有两个为true）
   previewImage: function previewImage(e) {
     console.log(e);
     var pictureList = e.currentTarget.dataset.src;
@@ -40,20 +51,25 @@ exports.default = Page({
     });
   },
   openPopup: function openPopup(e) {
-    // console.log(e)
-    var title = e.currentTarget.dataset.title;
-    // console.log(this.data.column11);
-    var allData = this.data.column11;
+    //  console.log(e);
+    var id = e.currentTarget.dataset.title;
+    // console.log(id);
+    var allData = this.data.column;
+    // console.log(allData);
     for (var i = 0; i < allData.length; i++) {
-      if (title == allData[i].title) {
-        allData[i].show = true;
-        console.log(allData);
-        this.setData({
-          column11: allData
-        });
+      var ids = allData[i].content;
+      for (var j = 0; j < ids.length; j++) {
+        if (id == ids[j].id) {
+          ids[j].show = true;
+          // console.log(allData);
+          this.setData({
+            column: allData
+          });
+        }
       }
     }
   },
+  //点击事件，判断相同title的弹框显示出来 把data里的show 改为true
   handleChange11: function handleChange11(e) {
     var index = e.detail.index;
     this.setData({
@@ -75,6 +91,11 @@ exports.default = Page({
         url: "../detail/detail"
       });
     }
+    if (this.data.current == "1") {
+      wx.navigateTo({
+        url: "../detail/detail"
+      });
+    }
   },
   bindchange: function bindchange(e) {
     //轮播图发生改变
@@ -83,80 +104,74 @@ exports.default = Page({
     });
   },
   onShow: function onShow(e) {
-    var list = [{
-      column_name: "工具",
-      column_id: "11"
-    }, {
-      column_name: "电商",
-      column_id: "12"
-    }, {
-      column_name: "游戏",
-      column_id: "13"
-    }];
+    var _this = this;
 
-    this.setData({
-      listNames: list
-    });
-    var couul = [{
-      column_id: "11",
-      content: [{
-        icon_src: "../../img/quwei.png",
-        title: "趣味视力表",
-        desc: "有趣好玩的视力表，明星视力表",
-        QRcode_src: "https://media.ifanrusercontent.com/user_files/trochili/bb/84/bb842752479463c573e0b02864469ec23293fdd3-6681daa63a15d8a1c35a60bcdddc2ef16c17285e.jpg", //二维码图片地址
-        app_url: "http://fanyi.baidu.com", //跳转地址
-        status: "1", //status值为1，使用二维码图片地址；status值为2，使用跳转地址,
-        show: false
-      }, {
-        icon_src: "../../img/wenyi.png",
-        title: "文艺乎",
-        desc: "原来你和我一样，偶尔也会想要安静下来写点什么，或是翻着别人的五味杂陈轻笑人生，在繁杂又枯燥的日子里做着文艺的梦",
-        QRcode_src: "https://media.ifanrusercontent.com/user_files/trochili/67/b8/67b846c1128063409dc9906cc2509a555d98b1a8-abca61bf3212ec0eaa196da1248759e725e8b333.jpg",
-        app_url: "http://fanyi.baidu.com",
-        status: "1",
-        show: false
-      }, {
-        icon_src: "../../img/qiong.png",
-        title: "史上最囧挑战Lite",
-        desc: "史上最囧的挑战，史上最囧的答题，史上最难的时间",
-        QRcode_src: "https://media.ifanrusercontent.com/user_files/trochili/d0/81/d081f66746a1418ca95cf72f36b6f0183b2a0e6a-f4bad74529d3a2590ef0cf8728251a77aa0c0e53.jpg",
-        app_url: "http://fanyi.baidu.com",
-        status: "1",
-        show: false
-      }]
-    }, {
-      column_id: "12",
-      content: [{
-        icon_src: "http://fanyi.baidu.com/translate?aldt",
-        title: "趣味视力表",
-        desc: "有趣好玩的视力表，明星视力表",
-        QRcode_src: "http://fanyi.baidu.com/translate?a5564sd",
-        app_url: "http://fanyi.baidu.com",
-        status: "1",
-        show: false
-      }, {
-        icon_src: "http://fanyi.baidu.com/translate?aldt",
-        title: "趣味视力表",
-        desc: "有趣好玩的视力表，明星视力表",
-        QRcode_src: "http://fanyi.baidu.com/translate?a5564sd",
-        app_url: "http://fanyi.baidu.com",
-        status: "1",
-        show: false
-      }]
-    }];
-
-    // console.log(couul);
-    for (var i = 0; i < couul.length; i++) {
-      if (couul[i].column_id == "11") {
-        // console.log(couul[i].content.title);
-        this.setData({
-          column11: couul[i].content
+    wx.request({
+      url: 'https://mipro.quapp.cn/mini/menu/list',
+      data: {
+        "type": "1"
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function success(res) {
+        //  console.log(res.data.data);
+        var list = res.data.data;
+        _this.setData({
+          listNames: list
         });
-      } else if (couul[i].column_id == "12") {
-        console.log(couul[i].content);
-      } else if (couul[i].column_id == "13") {
-        console.log(couul[i].content);
+      },
+      fail: function fail(res) {
+        wx.showToast({
+          title: '获取列表失败，请稍后重试',
+          icon: 'none',
+          duration: 2000
+        });
       }
-    }
+    });
+    wx.request({
+      url: 'https://mipro.quapp.cn/mini/content/list  ',
+      data: {
+        "type": "2"
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function success(res) {
+        //  console.log(res.data.data);
+        var couul = res.data.data;
+        _this.setData({
+          column: couul
+        });
+        // console.log(couul);
+        for (var i = 0; i < couul.length; i++) {
+          if (couul[i].column_id == "11") {
+            // console.log(couul[i].content.title);
+            _this.setData({
+              column11: couul[i].content
+            });
+          } else if (couul[i].column_id == "12") {
+            // console.log(couul[i].content);
+            _this.setData({
+              column12: couul[i].content
+            });
+          } else if (couul[i].column_id == "13") {
+            // console.log(couul[i].content);
+            _this.setData({
+              column13: couul[i].content
+            });
+          }
+        }
+      },
+      fail: function fail(res) {
+        wx.showToast({
+          title: '获取列表失败，请稍后重试',
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    });
   }
 });
